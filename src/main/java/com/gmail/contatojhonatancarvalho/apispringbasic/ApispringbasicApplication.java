4c828815f6fd8641e47f96d3cff21a1094748185
Jhonatan Carvalho <contatojhonatancarvalho@gmail.com>
2020-07-17 16:23:54 -0300

    Criação Model Usuario e Repository EndPoint usuarios

---
+++ src/main/java/com/gmail/contatojhonatancarvalho/apispringbasic/ApispringbasicApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.contatojhonatancarvalho.apispringbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApispringbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApispringbasicApplication.class, args);
+	}
+
+}
